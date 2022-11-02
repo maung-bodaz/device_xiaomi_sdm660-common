0de3ab13e002633eab69716b7c2522ed08974c69
Maung Bodas <116504869+maung-bodaz@users.noreply.github.com>
2022-11-03 06:51:07 +0700

    sdm660-common: Silence clang warnings during kernel compile clang-11: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused-command-line-argument]

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,8 +69,8 @@ KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
-#TARGET_KERNEL_CLANG_COMPILE := true
-#TARGET_KERNEL_CLANG_VERSION := latest
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
