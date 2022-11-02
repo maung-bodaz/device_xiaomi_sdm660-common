9fb0d02966fbdbe6d0648ea7ee3fbbbe6feb407c
Maung Bodas <116504869+maung-bodaz@users.noreply.github.com>
2022-11-03 06:20:02 +0700

    sdm660-common: Always Build kernel with latest available Clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,12 +70,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 TARGET_KERNEL_CLANG_COMPILE := true
-
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
-
-TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_VERSION := latest
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
